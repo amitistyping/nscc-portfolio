58e389509a75c644cfe2462d0bed368e4ee34eba
Amitoj Singh <amitoj.dev@gmail.com>
2023-02-28 19:29:11 -0400

    added rel=noopener noreferrer

--- components/Contact.js
+++ components/Contact.js
@@ -9,9 +9,9 @@ const Contact = () => {
 				</h2>
 				<p className='text-lg py-2 leading-8 text-gray-800 dark:text-gray-200'>
                     If you want to get in touch with me, please{' '}
-					<a href='mailto:amitoj.dev@gmail.com' target="_blank" className='text-teal-600'>click here</a> to send me an email. You can checkout my{' '}
-					<a href='https://www.linkedin.com/in/amitcodes/' target="_blank" className='text-teal-600'>LinkedIn</a> and{' '}
-					<a href='https://github.com/amitistyping' target="_blank" className='text-teal-600'>Github</a> as well. I am currently living in Halifax, Nova Scotia.
+					<a href='mailto:amitoj.dev@gmail.com' target="_blank" rel="noopener noreferrer" className='text-teal-600'>click here</a> to send me an email. You can checkout my{' '}
+					<a href='https://www.linkedin.com/in/amitcodes/' target="_blank" rel="noopener noreferrer" className='text-teal-600'>LinkedIn</a> and{' '}
+					<a href='https://github.com/amitistyping' target="_blank" rel="noopener noreferrer" className='text-teal-600'>Github</a> as well. I am currently living in Halifax, Nova Scotia.
 				</p>
 			</div>
 		</section>
