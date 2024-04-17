0975252e9f68c949f19c4736f96850171a159771
Amit <amitoj.dev@gmail.com>
2024-04-17 14:39:24 -0300

    updated some responsiveness

--- components/Header.js
+++ components/Header.js
@@ -5,35 +5,35 @@ import { AiOutlineDownload } from 'react-icons/ai';
 
 const Header = () => {
 	return (
-		<div className='flex justify-center items-center'>
-			<div id='introductionContainer' className='w-4/6'>
-				<h2 className='text-7xl py-2 text-teal-600 font-bold dark:text-teal-400 md:text-8xl'>
+		<div className='flex flex-col md:flex-row justify-center items-center'>
+			<div id='introductionContainer' className='w-full md:w-4/6 px-4'>
+				<h2 className='text-4xl md:text-7xl lg:text-8xl py-2 text-teal-600 font-bold dark:text-teal-400'>
 					Hi, I am Amit<span className='wave'>👋</span>
 				</h2>
-				<p className='text-lg py-2 leading-8 text-gray-900 dark:text-gray-300'>
-					I&apos;m a junior web developer with almost 2 years of experience developing web apps for the
-					banking and SaaS industries. Right now, I&apos;m super excited to be pursuing the Full Stack
-					Application Development program at NSCC IT Campus in Halifax to keep growing my skills. So
-					take a look around, and if you have any questions, feel free to drop me a line!
+				<p className='text-base md:text-lg py-2 leading-8 text-gray-900 dark:text-gray-300'>
+					I&apos;m a junior web developer with almost 2 years of experience developing web apps for the banking and SaaS industries. Right now, I&apos;m super
+					excited to be pursuing the Full Stack Application Development program at NSCC IT Campus in Halifax to keep growing my skills. So take a look around,
+					and if you have any questions, feel free to drop me a line!
 				</p>
-				<div className='mt-5'>
+				<div className='mt-5 flex flex-wrap'>
 					<a
-						className='text-lg bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-3 border-none rounded-md mr-3'
+						className='text-base md:text-lg bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-3 border-none rounded-md mr-3 mb-2'
 						href='#contactContainer'
 					>
 						Contact📮
 					</a>
 					<a
-						className='text-lg bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-3 border-none rounded-md'
+						className='text-base md:text-lg bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-3 border-none rounded-md mb-2'
 						href='https://amitistyping.github.io/resumepdf/amitresumeletter.pdf'
-                        target="_blank" rel="noopener noreferrer"
+						target='_blank'
+						rel='noopener noreferrer'
 					>
 						Resume <AiOutlineDownload className='inline' />
 					</a>
 				</div>
 			</div>
-			<div id='imageContainer' className='w-2/6'>
-				<Image src={softwareDev} alt="developer clip art" />
+			<div id='imageContainer' className='w-9/12 md:w-2/6 px-4 py-4 md:py-0'>
+				<Image src={softwareDev} alt='Amit as a developer illustrated' layout='responsive' width={420} height={400} />
 			</div>
 		</div>
 	);
--- components/Nav.js
+++ components/Nav.js
@@ -4,38 +4,36 @@ import { AiOutlineDownload } from 'react-icons/ai';
 
 const Nav = ({ handleDarkModeChange, darkMode }) => {
 	return (
-		<nav className='py-10 mb-12 flex justify-between dark:text-white'>
-			<h1 className='text-4xl font-semibold'>Amitoj Singh</h1>
-			<ul className='flex items-center'>
+		<nav className='py-5 mb-12 flex justify-between items-center px-4 md:py-10 md:px-0 dark:text-white'>
+			<h1 className='text-2xl md:text-4xl font-semibold'>Amitoj Singh</h1>
+			<ul className='flex items-center text-sm md:text-base'>
 				<li>
-					<a className='text-lg text-gray-900 dark:text-gray-300 mr-8' href='#projectsContainer'>
+					<a className='mr-4 md:mr-8' href='#projectsContainer'>
 						Projects
 					</a>
 				</li>
 				<li>
-					<a className='text-lg text-gray-900 dark:text-gray-300 mr-8' href='#skillsContainer'>
+					<a className='mr-4 md:mr-8' href='#skillsContainer'>
 						Skills
 					</a>
 				</li>
 				<li>
-					<a className='text-lg text-gray-900 dark:text-gray-300 mr-8' href='#contactContainer'>
+					<a className='mr-4 md:mr-8' href='#contactContainer'>
 						Contact
 					</a>
 				</li>
 				<li>
 					<a
-						className='text-lg bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-3 border-none rounded-md mr-8'
+						className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-3 py-2 md:px-4 md:py-3 border-none rounded-md mr-4 md:mr-8'
 						href='https://amitistyping.github.io/resumepdf/amitresumeletter.pdf'
-                        target="_blank" rel="noopener noreferrer"
+						target='_blank'
+						rel='noopener noreferrer'
 					>
 						Resume <AiOutlineDownload className='inline' />
 					</a>
 				</li>
 				<li>
-					<BsFillMoonStarsFill
-						onClick={() => handleDarkModeChange(!darkMode)}
-						className=' cursor-pointer text-2xl'
-					/>
+					<BsFillMoonStarsFill onClick={() => handleDarkModeChange(!darkMode)} className='cursor-pointer text-xl md:text-2xl' />
 				</li>
 			</ul>
 		</nav>
